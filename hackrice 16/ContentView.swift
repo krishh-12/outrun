2f5b13ee119cbd371a5f99f42ab231161c9bff71
Amulya Bisaria <a.bisaria@tamu.edu>
2026-09-12 13:42:00 -0500

    Initial Commit
    Co-authored-by: Anish Koduri <anishkoduri@tamu.edu>
    Co-authored-by: Srivanth Rudrangi <snr2753@tamu.edu>
    Co-authored-by: Amulya Bisaria <a.bisaria@tamu.edu>

---
+++ hackrice 16/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hackrice 16
+//
+//  Created by Krish Hariharan on 9/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hackrice 16/hackrice_16App.swift
@@ -0,0 +1,17 @@
+//
+//  hackrice_16App.swift
+//  hackrice 16
+//
+//  Created by Krish Hariharan on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct hackrice_16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
